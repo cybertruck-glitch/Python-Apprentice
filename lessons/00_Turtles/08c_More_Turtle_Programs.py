f308b5f1a4c90c7857d86ea4b6f2b64fd863cccb
cybertruck-glitch <caylakolakovsky@gmail.com>
2024-09-24 20:56:27 +0000

    crying class

--- lessons/00_Turtles/08c_More_Turtle_Programs.py
+++ lessons/00_Turtles/08c_More_Turtle_Programs.py
@@ -14,3 +14,28 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)
+def set_background_image(window, image_name):
+    """Set the background image of the turtle window to the image with the given name."""
+
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+# Set up the screen
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(width=600, height=600)     # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+screen = turtle.Screen()                # Get the screen that tina is on
+set_background_image(screen, "emoji.png") # Set the background image of the screen
+
+turtle.exitonclick() 
